8a1a663f9422d0994a9f34be60162404e73cc2e5
apple <apple@192.168.1.20>
2019-01-18 21:34:02 -0600

    added url

--- src/marsRover/MarsApplication.java
+++ src/marsRover/MarsApplication.java
@@ -8,30 +8,31 @@ public class MarsApplication {
 //	Change the code in your StudentResults class to this: Now, only code
 //	within the StudentResults class can see these field variables. 
 //	To check, go back to your main method.
+	//read http://tutorials.jenkov.com/java/fields.html
 	public static void main(String[] arguments) {
 		spirit = new MarsRover();
-		spirit.status = "exploring";
-		spirit.speed = 2;
-		spirit.temperature = -60;
-
+//		spirit.status = "exploring";
+//		spirit.speed = 2;
+//		spirit.temperature = -60;
+//
 		spirit.showAttributes();
-
-		System.out.println("Increasing speed to 3.");
-
-		spirit.speed = 3;
-
-		spirit.showAttributes();
-
-		System.out.println("Changing temperature to -90.");
-
-		spirit.temperature = -90;
-
-		spirit.showAttributes();
-
-		System.out.println("Checking the temperature.");
-
+//
+//		System.out.println("Increasing speed to 3.");
+//
+//		spirit.speed = 3;
+//
+//		spirit.showAttributes();
+//
+//		System.out.println("Changing temperature to -90.");
+//
+//		spirit.temperature = -90;
+//
+//		spirit.showAttributes();
+//
+//		System.out.println("Checking the temperature.");
+//
 		spirit.checkTemperature();
-
+//
 		spirit.showAttributes();
 	}
 }
